808c4a64fbe7e6d73d2a74cc2d9f778b355670eb
vadik <vadikverein45@gmail.com>
2020-12-20 19:42:03 +0500

    ready

--- chat.py
+++ chat.py
@@ -27,8 +27,6 @@ class WSChat:
         await ws.prepare(request)
 
         async for msg in ws:
-            await self.cleanup()
-
             if msg[1] != 'ping':
                 print(msg)
                 print(msg[1], type(msg[1]))
@@ -37,47 +35,42 @@ class WSChat:
                 await ws.send_str('pong')
             else:
                 type_ = re.findall(r'"mtype":"(.+?)"', msg[1])[0]
-                print(type_)
                 await self.commands[type_](ws, msg[1])
 
-    async def send_all(self, id, type, msg='', receiver=''):
-        for client, ws in self.conns.items():
-            try:
-                if id != client:
-                    if type == 'user_in':
-                        await ws.send_json({'mtype': 'USER_ENTER', 'id': id})
+        await self.send_all(self.conns[ws], 'user_out')
+        del self.conns[ws]
 
-                    elif type == 'user_out':
-                        await ws.send_json({'mtype': 'USER_LEAVE', 'id': id})
+    async def send_all(self, id, type_, msg='', receiver=''):
+        for ws, client in self.conns.items():
+            if id != client:
+                print(client, type(client), receiver, type(receiver))
+                if type_ == 'user_in':
+                    await ws.send_json({'mtype': 'USER_ENTER', 'id': id})
 
-                    elif type == 'msg_all':
-                        await ws.send_json({'mtype': 'MSG', 'id': id,
-                                            'text': msg})
+                elif type_ == 'user_out':
+                    await ws.send_json({'mtype': 'USER_LEAVE', 'id': id})
 
-                    elif type == 'msg_to':
-                        await ws.send_json({'mtype': 'DM', 'id': id,
-                                            'text': msg})
-            except ConnectionResetError:
-                # delete inactive
-                self.dels.append(client)
+                elif type_ == 'msg_all':
+                    await ws.send_json({'mtype': 'MSG', 'id': id,
+                                        'text': msg})
+
+                elif type_ == 'msg_to' and receiver == client:
+                    await ws.send_json({'mtype': 'DM', 'id': id,
+                                        'text': msg})
 
     async def init(self, ws, msg):
         client = re.findall(r'"id":"(.+?)"', msg)[0]
-        self.conns[client] = ws
-        print(self.conns)
+        self.conns[ws] = client
         await self.send_all(client, 'user_in')
 
     async def text(self, ws, msg):
         client = re.findall(r'"id":"(.+?)"', msg)[0]
-        receiver = re.findall(r'"to":(.+?),', msg)[0]
+        receiver = re.findall(r'"to":"?(.+?)"?,', msg)[0]
         text = re.findall(r'"text":"(.+?)"}', msg)[0]
-
-    async def cleanup(self):
-        # unsafe??? idk
-        for i in self.dels:
-            await self.send_all(i, 'user_out')
-            del self.conns[i]
-        self.dels = []
+        if receiver == 'null':
+            await self.send_all(client, 'msg_all', text)
+        else:
+            await self.send_all(client, 'msg_to', text, receiver)
 
 
 if __name__ == '__main__':
